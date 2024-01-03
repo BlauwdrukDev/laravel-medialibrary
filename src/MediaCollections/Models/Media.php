e8f17e0ecf6b4a2342709a59c7a630992cd07fb5
ithuis <ithuis@users.noreply.github.com>
2024-01-03 15:40:34 +0000

    Fix styling

--- src/MediaCollections/Models/Media.php
+++ src/MediaCollections/Models/Media.php
@@ -9,14 +9,12 @@ use Illuminate\Contracts\Support\Responsable;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 //use Illuminate\Database\Eloquent\Model;
-use MongoDB\Laravel\Eloquent\Model;
-
-
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 use Illuminate\Mail\Attachment;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Collection;
+use MongoDB\Laravel\Eloquent\Model;
 use Spatie\MediaLibrary\Conversions\Conversion;
 use Spatie\MediaLibrary\Conversions\ConversionCollection;
 use Spatie\MediaLibrary\Conversions\ImageGenerators\ImageGeneratorFactory;
