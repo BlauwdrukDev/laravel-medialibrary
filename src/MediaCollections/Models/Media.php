91d947e598569fe672cbd3db80972225de34a7b1
ithuis <117562628+ithuis@users.noreply.github.com>
2025-04-15 12:35:51 +0000

    Fix styling

--- src/MediaCollections/Models/Media.php
+++ src/MediaCollections/Models/Media.php
@@ -10,7 +10,7 @@ use Illuminate\Contracts\Support\Responsable;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Collection as EloquentCollection;
-//use Illuminate\Database\Eloquent\Model;
+// use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 use Illuminate\Mail\Attachment;
